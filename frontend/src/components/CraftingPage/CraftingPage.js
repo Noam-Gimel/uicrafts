757153f72b22a847d3f639dd3d069c73cb44c3ed
Noam-Gimel <tanki8897@gmail.com>
2024-08-18 21:37:07 +0300

    Added frame and light switch to control background color

--- frontend/src/components/CraftingPage/CraftingPage.js
+++ frontend/src/components/CraftingPage/CraftingPage.js
@@ -4,21 +4,38 @@ import { SettingsBar } from "../Settings/settings";
 import { ExampleButton } from "../Buttons/ExampleButton/ExampleButton";
 import { ButtonProvider } from "../../context/ButtonContext";
 
-
 export function CraftingPage() {
-    
+    const [innerFrameColor, setInnerFrameColor] = useState('var(--widget-dark)');
 
-  return (
+    const onBackgroundChange = () => {
+        if(innerFrameColor === 'var(--widget-light)') {
+            setInnerFrameColor('var(--widget-dark)');
+        } else {
+            setInnerFrameColor('var(--widget-light)');
+        }
+    }
+
+    return (
     <ButtonProvider>
         <div className={cn.Container}>
             <div className={cn.buttonDesignWrapper}>
-                <ExampleButton />
+                <div className={cn.buttonFrame}>
+                    <div className={cn.switchContainer}>
+                        <label className={cn.lightSwitch}>
+                            <input type="checkbox" onChange={onBackgroundChange}/>
+                            <span className={cn.switchSlider}></span>
+                        </label>
+                    </div>
+                    <div className={cn.buttonInnerFrame} style={{ backgroundColor: innerFrameColor }}>
+                        <ExampleButton />
+                    </div>
+                </div>
             </div>
             <div className={cn.settingsBarWrapper}>
                 <SettingsBar />
             </div>
         </div>
     </ButtonProvider>
-  );
+    );
 }
 
